b8d7d1b7510257a33da19e683a77fd45a22b1e15
Scarlett Clark <sgclark@kde.org>
2017-03-20 11:01:21 -0700

    boost-system env

--- scripts/ring.sh
+++ scripts/ring.sh
@@ -3,8 +3,8 @@ set -e
 set -x
 export LD_LIBRARY_PATH=/opt/usr/lib:/opt/usr/library:/opt/usr/lib/x86_64-linux-gnu:/usr/lib:/usr/lib64
 export PKG_CONFIG="pkg-config --static"
-export CPATH=/opt/usr/include:/opt/usr/include/corvusoft:/opt/usr/include/boost:/usr/include
-export LDFLAGS="-L/opt/usr/library $LDFLAGS"
+export CPATH=/opt/usr/include:/opt/usr/include/corvusoft:/opt/usr/include/boost/system:/opt/usr/include/boost:/usr/include
+export LDFLAGS="-L/opt/usr/library -L/opt/usr/lib $LDFLAGS"
 cd contrib
 
 rm -rfv build
