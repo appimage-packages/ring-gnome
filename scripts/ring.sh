c5afb0d477545e0f206436fc636bb8d8b3ef4b5c
Scarlett Clark <sgclark@kde.org>
2017-03-21 11:09:44 -0700

    path to gtk include files

--- scripts/ring.sh
+++ scripts/ring.sh
@@ -3,10 +3,13 @@ set -e
 set -x
 export LD_LIBRARY_PATH=/opt/usr/lib:/opt/usr/library:/opt/usr/lib/x86_64-linux-gnu:/usr/lib:/usr/lib64
 export PKG_CONFIG="pkg-config --static"
-export CPATH=/opt/usr/include:/opt/usr/include/corvusoft:/opt/usr/include/boost/system:/opt/usr/include/boost:/usr/include
+export CPATH=/opt/usr/include:/opt/usr/include/gtk-3.0:/opt/usr/include/corvusoft:/opt/usr/include/boost/system:/opt/usr/include/boost:/usr/include
 export LDFLAGS="-L/opt/usr/library -L/opt/usr/lib $LDFLAGS"
 
 pkg-config --exists gtk+-3.0
+echo $?
+
+pkg-config --libs "gtk+-3.0 >= 3.10"
 cd contrib
 
 rm -rfv build
