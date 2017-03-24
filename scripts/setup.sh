f4c2b5b3eb087b7847084dbde9f66ce70c505cd0
Scarlett Clark <sgclark@kde.org>
2017-03-24 07:27:40 -0700

    build with -fPIC ( static shared mix )

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -19,6 +19,7 @@
 # License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 export PATH=/opt/usr/bin:/home/jenkins/.rbenv/bin:/home/jenkins/.rbenv/shims:$PATH
 export WORKSPACE=`pwd`
+export CFLAGS=-fPIC
 echo $WORKSPACE
 add-apt-repository -y ppa:ubuntu-toolchain-r/test && apt-get update && apt-get -y install gcc-4.9 g++-4.9
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
